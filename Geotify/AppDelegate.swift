ab58426b3820c930f073f0e4949977571ff99c15
Rachit Sengupta <srachit@gmail.com>
2016-04-24 16:57:14 -0700

    Reacting to Geofence Events

--- Geotify/AppDelegate.swift
+++ Geotify/AppDelegate.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
   var window: UIWindow?
 
+  let locationManager = CLLocationManager()
+  
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    locationManager.delegate = self
+    locationManager.requestAlwaysAuthorization()
     return true
   }
 
@@ -37,6 +42,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+  
+  func handleRegionEvent(region: CLRegion!){
+    print("Geofence triggered!")
+  }
+  
+  func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+    if region is CLCircularRegion {
+      handleRegionEvent(region)
+    }
+  }
+  
+  func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+    if region is CLCircularRegion {
+      handleRegionEvent(region)
+    }
   }
 }
 
--- Geotify/GeotificationsViewController.swift
+++ Geotify/GeotificationsViewController.swift
@@ -197,5 +197,16 @@ class GeotificationsViewController: UIViewController, AddGeotificationsViewContr
     }
   }
   
+  func locationManager(manager: CLLocationManager!, monitoringDidFailForRegion region: CLRegion!, withError error: NSError!)
+  {
+    print("Monitoring failed for region with identifier: \(region.identifier)")
+  }
+  
+  func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+    print("Location Manager failed with the following error: \(error)")
+  }
+  
+  
+  
 }
 
